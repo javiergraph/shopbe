da51e5fd3938c5cae6c621ac756c255a016c2e04
javiergraph <73653542+javiergraph@users.noreply.github.com>
2022-04-08 15:02:21 +0200

    Delete opendesing directory

--- opendesing/js/sweetalert.js
+++
@@ -1,6 +0,0 @@
-Swal.fire({
-    icon: 'error',
-    title: 'Sélection vide!',
-    text: 'Revenez à la page principale pour effectuer votre achat',
-    footer: '<a href="index.php">Page Principal</a>'
-})
